f0f3dabe23a3cd15f04701337b15e9aad0e8e1aa
lfederico <luigi.federico.rossi@gmail.com>
2019-06-20 11:55:07 +0100

    Create formatAxes.m

---
+++ formatAxes.m
@@ -0,0 +1,11 @@
+function formatAxes(ax)
+
+% set(gcf, 'Color', 'w');
+
+if nargin < 1
+    set (gca, 'Box', 'off', 'TickDir', 'out', 'TickLength', [0.015 0.015], 'Fontsize', 12)
+else
+    set (ax, 'Box', 'off', 'TickDir', 'out', 'TickLength', [0.015 0.015], 'Fontsize', 12)
+
+end
+end
