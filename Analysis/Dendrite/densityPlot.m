e3cb786a385dd3bf161d2c71e63c06a6c103c5d0
Naureen Ghani <32179063+naureeng@users.noreply.github.com>
2020-02-09 16:49:48 +0000

    Add files via upload

--- Analysis/Dendrite/densityPlot.m
+++ Analysis/Dendrite/densityPlot.m
@@ -38,3 +38,43 @@ xlabel('Circular Variance');
 ylabel('Counts');
 legend('unshuffled', 'shuffled');
 
+%% step 5: histogram for 100 shuffles in visual space [AE] 
+histCnts_r = cat( 3, histCnts_s{:} );
+histCnts_r = mean(histCnts_r, 3);
+avgAng_r = rad2deg( circ_mean( deg2rad( cell2mat (avgAng_s) )));
+obj1 = CircHist(histCnts_r, 'dataType', 'histogram', 'avgAng', avgAng_r);
+
+%% step 6: clean up histogram 
+obj1.colorBar = 'k'; % change color of bars
+obj1.polarAxs.ThetaZeroLocation = 'right'; % rotate the plot to have 0 on the right side
+obj1.colorAvgAng = [.5 .5 .5]; % change average-angle line color
+title(''); % remove title
+obj1.scaleBarSide = 'left'; % draw rho-axis on the left side of the plot
+% draw resultant vector r as arrow
+% delete(obj1.rH)
+rl = rlim; % get current limits
+obj1.setRLim([rl(1) 0.07]);
+obj1.polarAxs.ThetaAxis.MinorTickValues = []; % remove dotted tick-lines
+thetaticks(0:90:360); % change major ticks
+rticks(0:1); % change rho-axis tick-steps to remove all concentric circles 
+% update scale bar
+delete(obj1.scaleBar); % remove scale bar 
+set(gca, 'fontname', 'Te X Gyre Heros'); % due to Linux compatability issue with Helvetica font]
+
+%% step 7: plot prefOri arrow
+obj1.drawArrow( 90 , [], 'HeadWidth', 10, 'LineWidth', 2, 'Color', 'b' );
+obj1.drawArrow( 270 , [], 'HeadWidth', 10, 'LineWidth', 2, 'Color', 'b' );
+
+%% step 8: plot avgAng arrow
+obj1.drawArrow(obj1.avgAng, [] , 'HeadWidth', 10, 'LineWidth', 2, 'Color', 'k')
+obj1.drawArrow(- (180 - obj1.avgAng) , [] , 'HeadWidth', 10, 'LineWidth', 2, 'Color', 'k')
+
+%% step 9: save polar plot
+print( 'AE_shuffle_hist', '-dpng' );
+close;
+
+
+
+
+
+
